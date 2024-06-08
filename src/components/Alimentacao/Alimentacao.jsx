ecf37768817cc7773ee97ad8d139dd1bb0b17bfe
Pedro Matsushita <pedromatsushita132@gmail.com>
2024-06-08 17:49:53 -0300

    Implementação do código Alimentacao.jsx

--- src/components/Alimentacao/Alimentacao.jsx
+++ src/components/Alimentacao/Alimentacao.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import style from './../Alimentacao/Alimentacao.module.css';
+import styles from '../Alimentacao/Alimentacao.module.css';
+import * as React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import TituloPagina from '../../components/TituloPagina/TituloPagina';
 
 const Alimentacao = () => {
+
+  const dadosDoacao = [
+    {
+      "id": 1,
+      "tipo": "Leite e Ovos",
+      "nome": "Abrigo 1",
+      "endereco": "Rua Prof. José Amarino dos Reis, 392",
+      "telefone": "(81) 98888-7777",
+      "email": "exemplo@gmail.com",
+    },
+    {
+      "id": 2,
+      "tipo": "Cesta Básica",
+      "nome": "Abrigo 2",
+      "endereco": "Rua Prof. José Amarino dos Reis, 392",
+      "telefone": "(81) 98888-7777",
+      "email": "exemplo@gmail.com",
+    },
+  ];
+
   return (
-    <div>Alimentacao</div>
-  )
+    <div>
+      <Navbar />
+      <TituloPagina titulo='Alimentação' />
+    
+      <div className={styles.conjuntoCards}>
+        {dadosDoacao.map((doacao, index) => (
+            <div key={index} className={styles.CardContatos}>
+              <h3>{doacao.tipo}</h3>
+              <p>{doacao.nome}</p>
+              <p>{doacao.endereco}</p>
+              <p>{doacao.telefone}</p>
+              <p>{doacao.email}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
-export default Alimentacao
+export default Alimentacao;
