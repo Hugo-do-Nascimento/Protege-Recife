bb660fa75af1fa0a638064e6414997792c204aae
Pedro Matsushita <pedromatsushita132@gmail.com>
2024-06-08 18:00:49 -0300

    Mudança CardContatos

--- src/components/CardContatos/CardContatos.jsx
+++ src/components/CardContatos/CardContatos.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './../CardContatos/CardContatos.module.css';
+import { Button } from '@mui/material';
 
 const CardContatos = ({ nomeOrgao, numeroContato }) => {
   return (
     <div className={styles.CardContatos}>
         <h3>{nomeOrgao}</h3>
         <p>{numeroContato}</p>
+        <a href={tel:${numeroContato}}>
+        <Button variant="contained">Ligar</Button>
+        </a>
     </div>
   )
 }
