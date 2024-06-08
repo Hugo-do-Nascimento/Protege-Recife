8d029a68e30a998a7d47a6d4cc7280eef78192ce
Pedro Matsushita <pedromatsushita132@gmail.com>
2024-06-08 17:58:28 -0300

    Mudança Calcados de doação e implementação do Css

--- src/components/Calcados/Calcados.jsx
+++ src/components/Calcados/Calcados.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import styles from './../Calcados/Calcados.module.css';
+import Navbar from '../Navbar/Navbar';
+import TituloPagina from '../TituloPagina/TituloPagina';
 
 const Calcados = () => {
+
+  const dadosDoacao = [
+    {
+      "id": 1,
+      "tipo": "Sandálias de Crianças",
+      "nome": "Abrigo 1",
+      "endereco": "Rua Prof. José Amarino dos Reis, 392",
+      "telefone": "(81) 98888-7777",
+      "email": "exemplo@gmail.com",
+    },
+    {
+      "id": 2,
+      "tipo": "Sapatos",
+      "nome": "Abrigo 2",
+      "endereco": "Rua Prof. José Amarino dos Reis, 392",
+      "telefone": "(81) 98888-7777",
+      "email": "exemplo@gmail.com",
+    },
+  ];
+
   return (
-    <div>Calcados</div>
+    <div>
+      <Navbar />
+      <TituloPagina titulo='Calçados'/>
+
+      <div className={styles.conjuntoCards}>
+        {dadosDoacao.map((doacao, index) => (
+            <div key={index} className={styles.CardContatos}>
+              <h3>{doacao.tipo}</h3>
+              <p>{doacao.nome}</p>
+              <p>{doacao.endereco}</p>
+              <p>{doacao.telefone}</p>
+              <p>{doacao.email}</p>
+            </div>
+          ))}
+      </div>
+
+    </div>
   )
 }
 
