50dd24cd48e658244c7c7f454656fb91a1beab81
Mariana dos Anjos <mariana.00000850248@unicap.br>
2024-06-09 14:10:17 -0300

    jsx de cadastro

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from 'react';
 import style from './Cadastro.module.css';
-import ErrorMEssage from '../../components/ErrorMEssage';
 import Modal from '../../components/Modal';
 import useAuthentication from '../../hook/useAuthentication';
-
+import InputMask from 'react-input-mask';
 import logo from '../../assets/logoL.svg';
+import { Link } from 'react-router-dom';
 
 const Cadastro = () => {
     const [displayName, setDisplayName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [qtdCriancas, setQtdCriancas] = useState("");
+    const [qtdAdultos, setQtdAdultos] = useState("");
     const [error, setError] = useState("");
 
     const { handleCadastro, erro, loading } = useAuthentication();
@@ -49,7 +52,7 @@ const Cadastro = () => {
                     type='text'
                     name='displayNome'
                     required
-                    placeholder='    Insira seu nome'
+                    placeholder='Insira seu nome'
                     value={displayName}
                     onChange={(e) => setDisplayName(e.target.value)}
                 />
@@ -60,7 +63,7 @@ const Cadastro = () => {
                     type='email'
                     name='email'
                     required
-                    placeholder='   Insira seu email'
+                    placeholder='Insira seu email'
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
@@ -71,7 +74,7 @@ const Cadastro = () => {
                     type='password'
                     name='password'
                     required
-                    placeholder='   Insira sua senha'
+                    placeholder='Insira sua senha'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
@@ -82,13 +85,51 @@ const Cadastro = () => {
                     type='password'
                     name='confrimPassword'
                     required
-                    placeholder='   Insira sua senha'
+                    placeholder='Insira sua senha'
                     value={confirmPassword}
                     onChange={(e) => setConfirmPassword(e.target.value)}
                 />
             </label>
+            <label>
+                <span>Número de Telefone:</span>
+                <InputMask
+                    mask="(99) 99999-9999"
+                    value={telefone}
+                    onChange={(e) => setTelefone(e.target.value)}
+                >
+                    {() => <input 
+                            type='tel' 
+                            name='telefone' 
+                            required 
+                            placeholder='81 98181-8282' 
+                            />}
+                </InputMask>
+            </label>
+            <label>
+                <span>Quantidade de Crianças:</span>
+                <input 
+                    type='number'
+                    name='qtdCriancas'
+                    required
+                    placeholder='Insira a quantidade de crianças'
+                    value={qtdCriancas}
+                    onChange={(e) => setQtdCriancas(e.target.value)}
+                />
+            </label>
+            <label>
+                <span>Quantidade de Adultos:</span>
+                <input 
+                    type='number'
+                    name='qtdAdultos'
+                    required
+                    placeholder='Insira a quantidade de Adultos'
+                    value={qtdAdultos}
+                    onChange={(e) => setQtdAdultos(e.target.value)}
+                />
+            </label>
             {!loading && <button className={style.btn}>Cadastrar</button>}
             {loading && <button className={style.btn} disabled>Aguarde...</button>}
+            <Link className={style.link} to='/login'>voltar ao Login</Link>
             </form>
             {error && <Modal message={error} onClose={() => setError("")} />}
         </div>
