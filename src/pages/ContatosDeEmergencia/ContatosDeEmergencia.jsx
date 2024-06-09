109f96b0da1f9fa5efc931f97c3f3db0776fbc8a
João Miguel <81876832+jjj-miguel@users.noreply.github.com>
2024-06-09 19:10:07 +0000

    modificando ContatosDeEmergencia

--- src/pages/ContatosDeEmergencia/ContatosDeEmergencia.jsx
+++ src/pages/ContatosDeEmergencia/ContatosDeEmergencia.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import styles from './../ContatosDeEmergencia/ContatosDeEmergencia.module.css';
 import CardContatos from '../../components/CardContatos/CardContatos';
 import Navbar from '../../components/Navbar/Navbar';
-
+import TituloPagina from '../../components/TituloPagina/TituloPagina';
 const ContatosDeEmergencia = () => {
+
   return (
     <div className={styles.container}>
       <Navbar />
-      <h1 className={styles.h1}>Contados de Emergência</h1>
-      <CardContatos nomeOrgao="Defesa Civil Recife" numeroContato="0800 081 3400" />
+      <TituloPagina titulo="Contatos de Emergência" />
+      <CardContatos nomeOrgao="Defesa Civil Recife (24h)" numeroContato="0800 081 3400" />
       <CardContatos nomeOrgao="Defesa Civil PE" numeroContato="199" />
-      <CardContatos nomeOrgao="SAMU" numeroContato="192" />
-      <CardContatos nomeOrgao="Bombeiros" numeroContato="193" />
-      <CardContatos nomeOrgao="Polícia Militar" numeroContato="190" />
+      <CardContatos nomeOrgao='CTTU' numeroContato='0800 081 1078'/>
+      <CardContatos nomeOrgao='Emlurb' numeroContato='156'/>
+      <CardContatos nomeOrgao='Neoenergia' numeroContato='116'/>
+      <CardContatos nomeOrgao="SAMU" numeroContato="192"/>
+      <CardContatos nomeOrgao="Corpo de Bombeiros" numeroContato="193" />
+      <CardContatos nomeOrgao="Polícia Militar PE" numeroContato="180"/>
     </div>
   )
 }
 
-export default ContatosDeEmergencia
+export default ContatosDeEmergencia;
